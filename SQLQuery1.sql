4b15ccf5fa47a1d85a2af9ed23c9378f99aaff60
palomara <palomarangelll@hotmail.com>
2018-03-08 15:20:44 -0300

    foreign key

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,8 +2,13 @@ use master
 go
 
 drop database bancoaula
+go
+
 create database bancoaula
+go
+
 use bancoaula
+go
 
 
 create table tb_cliente (
@@ -22,7 +27,7 @@ create table tb_hardware (
 
 create table tb_vendas (
 	id_venda	int primary key identity (1,1),
-	id_cliente	int,
+	id_cliente	int, -- foreign key,
 	data		date,
 	vlrtotal	decimal,
 	desconto	decimal,
@@ -30,12 +35,22 @@ create table tb_vendas (
 
 create table vendas_itens (
 	id_item		int primary key identity (1,1),
-	id_venda	int,
+	id_venda	int, 
 	id_hardware int,
 	qtde_item	int,
 	total_item	int)
-go 
-exec sp_databases
+ --go 
+ --exec sp_databases
+ --go
+ --exec sp_tables
+--exec sp_help tb_cliente
+
 go
-exec sp_tables
-exec sp_help tb_cliente
+alter table tb_vendas add constraint fk_idcliente foreign key (id_cliente)references tb_cliente (id_cliente) 
+go
+alter table vendas_itens add constraint fk_idvenda foreign key (id_venda)references tb_vendas (id_venda) 
+go
+alter table tb_hardware add constraint fk_idhardware foreign key (id_hardware) references tb_hardware (id_hardware)
+go
+insert into tb_vendas (id_cliente)
+values(20)
